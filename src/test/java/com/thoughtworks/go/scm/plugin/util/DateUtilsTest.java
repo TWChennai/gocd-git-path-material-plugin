fc15511e3cd7500868b4e39a7073b206b9608a65
Krishnaswamy Subramanian <jskswamy@gmail.com>
2016-09-28 16:19:48 +0530

    Fixing timezone sensitive test

--- src/test/java/com/thoughtworks/go/scm/plugin/util/DateUtilsTest.java
+++ src/test/java/com/thoughtworks/go/scm/plugin/util/DateUtilsTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
@@ -13,7 +14,10 @@ public class DateUtilsTest {
     @Test
     public void parseISO8601() {
         Date actual = DateUtils.parseISO8601("1994-11-05T08:15:30-05:00");
-        Date expected = new GregorianCalendar(1994, 10, 5, 13, 15, 30).getTime();
+        GregorianCalendar gregorianCalendar = new GregorianCalendar(1994, 10, 5, 13, 15, 30);
+        gregorianCalendar.setTimeZone(TimeZone.getTimeZone("Etc/UTC"));
+
+        Date expected = gregorianCalendar.getTime();
         assertThat(actual, is(equalTo(expected)));
     }
 }
