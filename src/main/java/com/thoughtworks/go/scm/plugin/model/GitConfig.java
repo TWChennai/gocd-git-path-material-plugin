c86d875a35bbd4ba733bf9de6d75c70d76bfdb8a
Krishnaswamy Subramanian <jskswamy@gmail.com>
2016-10-08 22:30:56 +0530

    Add necessary test for GitConfig

--- src/main/java/com/thoughtworks/go/scm/plugin/model/GitConfig.java
+++ src/main/java/com/thoughtworks/go/scm/plugin/model/GitConfig.java
@@ -1,27 +1,25 @@
 package com.thoughtworks.go.scm.plugin.model;
 
-import com.thoughtworks.go.plugin.api.logging.Logger;
+import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
 import com.thoughtworks.go.scm.plugin.util.JsonUtils;
 import com.thoughtworks.go.scm.plugin.util.StringUtils;
-import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
 import java.util.Map;
 
-@Getter
 @ToString
 @EqualsAndHashCode
+@Getter
 public class GitConfig {
 
     private String url;
     private String username;
     private String password;
     private String branch;
-    private boolean subModule = false;
+
     private boolean recursiveSubModuleUpdate = true;
-    private boolean shallowClone = false;
 
     public static GitConfig create(GoPluginApiRequest apiRequest) {
         Map<String, String> configuration = JsonUtils.parseScmConfiguration(apiRequest);
@@ -29,16 +27,15 @@ public class GitConfig {
     }
 
     public GitConfig(String url, String username, String password, String branch) {
-        this(url, username, password, branch, true, false);
+        this(url, username, password, branch, true);
     }
 
-    private GitConfig(String url, String username, String password, String branch, boolean recursiveSubModuleUpdate, boolean shallowClone) {
+    private GitConfig(String url, String username, String password, String branch, boolean recursiveSubModuleUpdate) {
         this.url = url;
         this.username = username;
         this.password = password;
         this.branch = branch;
         this.recursiveSubModuleUpdate = recursiveSubModuleUpdate;
-        this.shallowClone = shallowClone;
     }
 
     public boolean isRemoteUrl() {
@@ -56,16 +53,12 @@ public class GitConfig {
         return getUrl();
     }
 
-    public String getUrlWithCredentials() {
-        String[] parts = url.split("://");
-        return String.format("%s://%s:%s@%s", parts[0], username, password, parts[1]);
-    }
-
     public String getEffectiveBranch() {
         return StringUtils.isEmpty(branch) ? "master" : branch;
     }
 
-    public boolean isRecursiveSubModuleUpdate() {
-        return recursiveSubModuleUpdate;
+    private String getUrlWithCredentials() {
+        String[] parts = url.split("://");
+        return String.format("%s://%s:%s@%s", parts[0], username, password, parts[1]);
     }
 }
