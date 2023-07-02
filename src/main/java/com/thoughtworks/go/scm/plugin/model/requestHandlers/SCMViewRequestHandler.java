e264b0d6c39f2844d9c5ae340ee589f07734153b
Chad Wilson <chadw@thoughtworks.com>
2023-07-02 23:06:44 +0800

    Remove unnecessary use of commons-io

--- src/main/java/com/thoughtworks/go/scm/plugin/model/requestHandlers/SCMViewRequestHandler.java
+++ src/main/java/com/thoughtworks/go/scm/plugin/model/requestHandlers/SCMViewRequestHandler.java
@@ -3,12 +3,13 @@ package com.thoughtworks.go.scm.plugin.model.requestHandlers;
 import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
 import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
 import com.thoughtworks.go.scm.plugin.util.JsonUtils;
-import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class SCMViewRequestHandler implements RequestHandler {
     public static final String PLUGIN_NAME = "Git Path";
@@ -18,11 +19,17 @@ public class SCMViewRequestHandler implements RequestHandler {
         Map<String, Object> response = new HashMap<>();
         response.put("displayValue", PLUGIN_NAME);
         try {
-            response.put("template", IOUtils.toString(getClass().getResourceAsStream("/scm.template.html"), StandardCharsets.UTF_8));
+            response.put("template", resourceAsString());
             return JsonUtils.renderSuccessApiResponse(response);
         } catch (IOException e) {
             String message = String.format("Failed to find template: %s", e.getMessage());
             return JsonUtils.renderErrorApiResponse(message);
+        }
+    }
+
+    private String resourceAsString() throws IOException {
+        try (InputStream is = Objects.requireNonNull(getClass().getResourceAsStream("/scm.template.html"))) {
+            return new String(is.readAllBytes(), StandardCharsets.UTF_8);
         }
     }
 }
